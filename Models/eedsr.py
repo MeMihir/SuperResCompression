671571556990cbfc8fb63613dacf3751b323c94f
Mihir Pavuskar <pavuskarmihir@gmail.com>
2020-10-30 06:45:02 +0000

    EEDSR setup done. bugs left

--- Models/eedsr.py
+++ Models/eedsr.py
@@ -2,6 +2,8 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, add, Conv2D, Input, Conv2DTranspose, UpSampling2D, concatenate, Activation
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint
+# import prepare_data as pd
+# import pandas as pd
 import numpy
 import math
 import cv2
@@ -35,9 +37,10 @@ class EEDSR():
 
     return model
 
+
   def model_EED(self):
     _input = Input(shape=(None, None, 1), name='input')
-    
+
     Feature = Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu')(_input)
     Feature_out = self.Res_block()(Feature)
 
@@ -72,7 +75,7 @@ class EEDSR():
     Multi_scale2d = Conv2D(16, (7, 1), strides=(1, 1),
                            padding='same', activation='relu')(Multi_scale2d)
 
-    Multi_scale2 = concatenate(inputs=[Multi_scale2a, Multi_scale2b, Multi_scale2c, Multi_scale2d])
+    Multi_scale2 = concatenate(inputs=[Multi_scale2a, Multi_scale2b, Multi_scale2c, Multi_scale2d], axis=0)
 
     out = Conv2D(1, (1, 1), strides=(1, 1), padding='same', activation='relu')(Multi_scale2)
     model = Model(_input, out)
@@ -87,21 +90,20 @@ class EEDSR():
 
     model = Model(_input, _EEDS)
     model.compile(optimizer=Adam(lr=0.0003), loss='mse')
+    self.model = model
+    model.load_weights("./")
     return model
 
   def EEDS_predict(self):
-    IMG_NAME = "./input.jpg"
     INPUT_NAME = "input.jpg"
-    OUTPUT_NAME = "EEDS_pre.jpg"
 
-    label = cv2.imread(IMG_NAME)
+    label = cv2.imread(self.image_path)
     shape = label.shape
 
     img = cv2.resize(label, (int(shape[1] / scale), int(shape[0] / scale)), cv2.INTER_CUBIC)
     cv2.imwrite(INPUT_NAME, img)
 
     EEDS = self.model_EEDS()
-    EEDS.load_weights("./EEDS_check.h5")
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     Y = numpy.zeros((1, img.shape[0], img.shape[1], 1))
@@ -113,10 +115,10 @@ class EEDSR():
     pre = numpy.uint8(pre)
     img[:, :, 0] = pre[0, :, :, 0]
     img = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
-    cv2.imwrite(OUTPUT_NAME, img)
+    cv2.imwrite('../', img)
 
     # psnr calculation:
-    # im1 = cv2.imread(IMG_NAME, cv2.IMREAD_COLOR)
+    # im1 = cv2.imread(self.image_path, cv2.IMREAD_COLOR)
     # im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2YCrCb)
     # im2 = cv2.imread(INPUT_NAME, cv2.IMREAD_COLOR)
     # im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2YCrCb)
